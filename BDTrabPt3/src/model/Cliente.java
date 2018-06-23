c2ab427e1174ce1eab0051295eb767cb0333f9b3
TMTiagoT <tiagosmi@hotmail.com>
2018-06-22 22:29:25 -0300

    commit inicial

---
+++ BDTrabPt3/src/model/Cliente.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *CREATE TABLE CLIENTE (
+    CPF CHAR(14) NOT NULL,
+    NOME VARCHAR2(40) NOT NULL,
+    EMAIL VARCHAR2(48),
+    EMPRESA VARCHAR2(40),
+    TELEFONE1 CHAR(20) NOT NULL,
+    TELEFONE2 CHAR(20),
+    CONSTRAINT PK_CLIENTE PRIMARY KEY(CPF)
+); */
+
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private String email;
+    private String empresa;
+    private String telefone1;
+    private String telefone2;
+
+    public Cliente(String cpf, String nome, String email, String empresa, String telefone1, String telefone2) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.empresa = empresa;
+        this.telefone1 = telefone1;
+        this.telefone2 = telefone2;
+    }
+    
+    @Override
+    public String toString() {
+        return ("CPF: " + this.cpf + " \tNome: " + this.nome + " \tEmail: " + ((this.email == null) ? ("") : (this.email)) 
+                + " \tEmpresa: " + ((this.empresa == null) ? ("") : (this.empresa)) + " \tTelefone 1: " + this.telefone1 
+                + " \tTelefone 2: " + ((this.telefone2 == null) ? ("") : (this.telefone2)));
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getTelefone1() {
+        return telefone1;
+    }
+
+    public void setTelefone1(String telefone1) {
+        this.telefone1 = telefone1;
+    }
+
+    public String getTelefone2() {
+        return telefone2;
+    }
+
+    public void setTelefone2(String telefone2) {
+        this.telefone2 = telefone2;
+    }
+    
+}
---
+++ BDTrabPt3/src/sgbd/ClienteDB.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgbd;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.Cliente;
+
+public class ClienteDB {
+
+    //selecionar todos os clientes
+    public ArrayList<Cliente> Cliente_SelectAll() {
+        ArrayList<Cliente> clientes = new ArrayList();
+        
+        Connection con = null;
+        try {
+            con = ConnectionSGBD.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT CPF, NOME, EMAIL, EMPRESA, TELEFONE1, TELEFONE2 FROM CLIENTE");
+
+            while (rs.next()) {
+                clientes.add(new Cliente(rs.getString("CPF"), rs.getString("NOME"), rs.getString("EMAIL"),
+                                    rs.getString("EMPRESA"), rs.getString("TELEFONE1"), rs.getString("TELEFONE2")));
+            }
+            
+            //con.commit(); //depois ver de desabilitar commit automatico, mas por enquanto eh melhor assim
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Erro no Cliente_SelectAll");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Erro no Cliente_SelectAll");
+        }
+        
+        ConnectionSGBD.CloseConnection(con);
+
+        return clientes;
+    }
+}
