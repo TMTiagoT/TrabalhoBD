184a5cf2b4f867c46077a326ede00b73fb400ee1
Ndriw <andre.andre831@hotmail.com>
2018-06-24 18:39:29 -0300

    sadfc

--- BDTrabPt3/src/hud/cliente/TelaInserirCliente.java
+++ BDTrabPt3/src/hud/cliente/TelaInserirCliente.java
@@ -198,42 +198,42 @@ public class TelaInserirCliente extends javax.swing.JInternalFrame {
     
     private void textField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField1ActionPerformed
         // TODO add your handling code here:
-        nome = textField1.getText();
-        System.out.println(nome);
     }//GEN-LAST:event_textField1ActionPerformed
 
     private void textField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField2ActionPerformed
         // TODO add your handling code here:
-        cpf = textField2.getText();
-        System.out.println(cpf);
     }//GEN-LAST:event_textField2ActionPerformed
 
     private void textField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField3ActionPerformed
         // TODO add your handling code here:
-        email = textField3.getText();
-        System.out.println(email);
     }//GEN-LAST:event_textField3ActionPerformed
 
     private void textField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField4ActionPerformed
         // TODO add your handling code here:
-        empresa = textField4.getText();
-        System.out.println(empresa);
     }//GEN-LAST:event_textField4ActionPerformed
 
     private void textField5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField5ActionPerformed
         // TODO add your handling code here:
-        telefone1 = Integer.parseInt(textField5.getText());
-        System.out.println(telefone1);
     }//GEN-LAST:event_textField5ActionPerformed
 
     private void textField6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField6ActionPerformed
         // TODO add your handling code here:
-        telefone2 = Integer.parseInt(textField6.getText());
-        System.out.println(telefone2);
     }//GEN-LAST:event_textField6ActionPerformed
 
     private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
         // TODO add your handling code here:
+        nome = textField1.getText();
+        System.out.println(nome);
+        cpf = textField2.getText();
+        System.out.println(cpf);
+        email = textField3.getText();
+        System.out.println(email);
+        empresa = textField4.getText();
+        System.out.println(empresa);
+        telefone1 = Integer.parseInt(textField5.getText());
+        System.out.println(telefone1);
+        telefone2 = Integer.parseInt(textField6.getText());
+        System.out.println(telefone2);
         Cliente cliente = new Cliente(cpf, nome, email, empresa, telefone1, telefone2);
         ClienteDB clientedb = new ClienteDB();
         clientedb.Cliente_Insert(cliente);
